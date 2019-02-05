f62c107563d857d22634351a95b97d71fb1b7986
crependormie <47205269+crependormie@users.noreply.github.com>
2019-02-05 23:36:16 +0300

    Update interface_finding.py

--- interface_finding.py
+++ interface_finding.py
@@ -1,94 +1,65 @@
 class Bcform(QWidget):
-
+  
   def __init__(self):
     super().__init__()
+    
+    self.initUI()
 
-self.initUI()
+  def initUI(self):
+    self.lbl1 = QLabel('Найден beacon-маячок:', self)
+    self.lbl1.move(150, 50)
 
-def initUI(self):
+    self.line1 = QLineEdit(self)
+    self.line1.setText(ibng.key_generation()[0])
+    self.line1.resize(385,20)
+    self.line1.move(35,100)
 
-self.lbl1 = QLabel('Найден beacon-маячок:', self)
+    self.btn = QPushButton("Расчитать дистанцию",self)
+    self.btn.setToolTip('Рассчитать дистанцию до найденного маячка?')
+    self.btn.resize(self.btn.sizeHint())
+    self.btn.move(140, 150)
+    self.btn.clicked.connect(self.buttonClicked)
 
-self.lbl1.move(150, 50)
+    self.lbl2 = QLabel('Расстояние до маячка:', self)
+    self.lbl2.move(150, 200)
+    self.line2 = QLineEdit(self)
+    self.lbl2.adjustSize()
+    self.line2.move(150,250)
 
-self.line1 = QLineEdit(self)
+    self.lbl3 = QLabel('метр', self)
+    self.lbl3.move(280, 250)
+  
+    self.resize(450, 350)
+    self.center()
 
-self.line1.setText(ibng.key_generation()[0])
+    self.setWindowTitle('iBeacon')
+    self.show()
 
-self.line1.resize(385,20)
+  def center(self):
+    
+    qr = self.frameGeometry()
+    cp = QDesktopWidget().availableGeometry().center()
+    qr.moveCenter(cp)
+    self.move(qr.topLeft())
 
-self.line1.move(35,100)
+  def closeEvent(self, event):
+    
+    reply = QMessageBox.question(self, 'Message', "Are you sure to quit?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+    
+    if reply == QMessageBox.Yes:
+      event.accept()
+    else:
+      event.ignore()
 
-self.btn = QPushButton("Расчитать дистанцию",self)
+  def buttonClicked(self):
+    
+    key,tx_power = ibng.key_generation()
+    rssi = ibng.get_rssi(tx_power)
 
-self.btn.setToolTip('Рассчитать дистанцию до найденного маячка?')
-
-self.btn.resize(self.btn.sizeHint())
-
-self.btn.move(140, 150)
-
-self.btn.clicked.connect(self.buttonClicked)
-
-self.lbl2 = QLabel('Расстояние до маячка:', self)
-
-self.lbl2.move(150, 200)
-
-self.line2 = QLineEdit(self)
-
-self.lbl2.adjustSize()
-
-self.line2.move(150,250)
-
-self.lbl3 = QLabel('метр', self)
-
-self.lbl3.move(280, 250)
-
-self.resize(450, 350)
-
-self.center()
-
-self.setWindowTitle('iBeacon')
-
-self.show()
-
-def center(self):
-
-qr = self.frameGeometry()
-
-cp = QDesktopWidget().availableGeometry().center()
-
-qr.moveCenter(cp)
-
-self.move(qr.topLeft())
-
-def closeEvent(self, event):
-
-reply = QMessageBox.question(self, 'Message',
-
-"Are you sure to quit?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-
-if reply == QMessageBox.Yes:
-
-event.accept()
-
-else:
-
-event.ignore()
-
-def buttonClicked(self):
-
-key,tx_power = ibng.key_generation()
-
-rssi = ibng.get_rssi(tx_power)
-
-dictance = ibng.get_distance(rssi,tx_power)
-
-self.line2.setText(str(dictance))
-
+    dictance = ibng.get_distance(rssi,tx_power)
+    self.line2.setText(str(dictance))
+    
 if __name__ == '__main__':
-
-app = QApplication(sys.argv)
-
-ex = Bcform()
-
-sys.exit(app.exec_())
+  app = QApplication(sys.argv)
+  ex = Bcform()
+  sys.exit(app.exec_())
